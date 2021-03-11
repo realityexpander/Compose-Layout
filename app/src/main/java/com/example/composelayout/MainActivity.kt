35c701eba334ef92eb338a35b888bbbba9ca9aeb
Chris Athanas <chris@crowdskout.com>
2021-03-10 21:29:21 -0600

    Custom layout

--- app/src/main/java/com/example/composelayout/MainActivity.kt
+++ app/src/main/java/com/example/composelayout/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.composelayout
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
@@ -24,6 +25,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.AlignmentLine
 import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.layout.layout
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -65,9 +67,64 @@ fun LayoutsCodelab() {
         BodyContent(Modifier
             .padding(innerPadding)
             .padding(8.dp))
+
+        BodyContentCustomLayout(Modifier
+            .padding(innerPadding)
+            .padding(8.dp))
     }
 }
 
+/*
+    Custom Layout
+ */
+@Preview(showBackground = true, backgroundColor = 0xFFFFFFFF,
+    uiMode = Configuration.UI_MODE_TYPE_NORMAL)
+@Composable
+fun BodyContentCustomLayout(modifier: Modifier = Modifier) {
+    MyOwnColumn(modifier.padding(8.dp)) {
+        Text("MyOwnColumn")
+        Text("places items")
+        Text("vertically.")
+        Text("We've done it by hand!")
+    }
+}
+
+@Composable
+fun MyOwnColumn(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+        // Don't constrain child views further, measure them with given constraints
+        // List of measured children
+        val placeables = measurables.map { measurable ->
+            // Measure each child
+            measurable.measure(constraints)
+        }
+
+        // Track the y co-ord we have placed children up to
+        var yPosition = 0
+
+        // Set the size of the layout as big as it can
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            // Place children in the parent layout
+            placeables.forEach { placeable ->
+                // Position item on the screen
+                placeable.placeRelative(x = 0, y = yPosition)
+
+                // Record the y co-ord placed up to
+                yPosition += placeable.height
+            }
+        }
+    }
+}
+
+/*
+    Custom baseline
+ */
 fun Modifier.firstBaselineToTop(
     firstBaselineToTop: Dp
 ) = Modifier.layout { measurable, constraints ->
@@ -101,6 +158,9 @@ fun TextWithNormalPaddingPreview() {
     }
 }
 
+/*
+    ImageList & Control scrolling
+ */
 
 @Composable
 private fun BodyContent(modifier: Modifier = Modifier) {
