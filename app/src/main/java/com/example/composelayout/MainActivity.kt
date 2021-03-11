f3725617681fa05630ffce7b7933d37c8c85371c
Chris Athanas <chris@crowdskout.com>
2021-03-10 21:18:12 -0600

    Custom Baseline offset

--- app/src/main/java/com/example/composelayout/MainActivity.kt
+++ app/src/main/java/com/example/composelayout/MainActivity.kt
@@ -22,8 +22,12 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.composelayout.ui.theme.ComposeLayoutTheme
 import dev.chrisbanes.accompanist.coil.CoilImage
@@ -63,6 +67,40 @@ fun LayoutsCodelab() {
             .padding(8.dp))
     }
 }
+
+fun Modifier.firstBaselineToTop(
+    firstBaselineToTop: Dp
+) = Modifier.layout { measurable, constraints ->
+    val placeable = measurable.measure(constraints)
+
+    // Check the composable has a first baseline
+    check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+    val firstBaseline = placeable[FirstBaseline]
+
+    val placeableY = firstBaselineToTop.roundToPx() - firstBaseline
+    val height = placeable.height + placeableY
+    layout(placeable.width, height) {
+        // Where the composable gets placed
+        placeable.placeRelative(0, placeableY)
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFFFFFFF)
+@Composable
+fun TextWithPaddingToBaselinePreview() {
+    ComposeLayoutTheme {
+        Text("Hi there!", Modifier.firstBaselineToTop(32.dp))
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFFFFFFF)
+@Composable
+fun TextWithNormalPaddingPreview() {
+    ComposeLayoutTheme {
+        Text("Hi there!", Modifier.padding(top = 32.dp))
+    }
+}
+
 
 @Composable
 private fun BodyContent(modifier: Modifier = Modifier) {
